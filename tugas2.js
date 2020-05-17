4c6060e9e2679163eefce74e70e081a39c52f512
Hayatuddin <hayatuddinnur53@gmail.com>
2020-05-17 22:38:16 +0700

    TUGAS 2 JAVASCRIPT
    Tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,8 @@
+var angka = 30;
+var massage="Hallo Coding";
+var tinggi =true;
+var kosong= null;
+var juml_barang;
+var nomor_urut=[1,2,3,4,5];
+
+console.log (angka, massage, tinggi, kosong,juml_barang, nomor_urut[4] );
